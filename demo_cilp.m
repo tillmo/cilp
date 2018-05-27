4582d55cac66002e16a923c0d353f90430f73d90
Till Mossakowski <till@iws.cs.ovgu.de>
2018-05-27 10:25:25 +0200

    training of cilp-generated network

--- demo_cilp.m
+++ demo_cilp.m
@@ -5,4 +5,12 @@ b=1
 # translate logical program to neural network
 [theta1, theta2, outin, amin, inlits, outlits] = cilp(p,b)
 # compute least fixpoint of tp
-x = iter_tp(theta1, theta2, outin, amin, b)
+x1 = iter_tp(theta1, theta2, outin, amin, b)
+# learning
+clause = ["+D";"+B";"-A"]
+in = lits_to_vector(inlits,clause)
+out = lits_to_vector(outlits,clause)
+[thetan1,thetan2] = nn_train(in,out,0.0001,b,100000,0.12,theta1, theta2)
+# now use the trained network
+x2 = iter_tp(thetan1, thetan2, outin, amin, b)
+
