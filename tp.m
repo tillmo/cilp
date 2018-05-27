4886ff3c23054c53f1e284b9091bcde185237b69
Till Mossakowski <till@iws.cs.ovgu.de>
2018-05-27 08:46:36 +0200

    improved comments

--- tp.m
+++ tp.m
@@ -6,7 +6,7 @@ function out = tp(x,theta1, theta2, outin, amin,b = 1)
   # threshold amin distinguishes between true and false
   x3 = (x2 > amin) * 2 - 1;
   # x3=x2; # use this if you want to try a more continuous behaviour
-  # use old input, and overwrite it with new output whenever it fits
+  # use old input or overwrite it with new output, depending on what is given in the outpout layer
   overwrite = sum(outin',1);
   stay = 1-overwrite;
   out = (diag(stay)*x'+diag(overwrite)*x3')';
