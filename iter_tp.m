f43929fde046f069daa24a9e17eaa4479b847144
Till Mossakowski <till@iws.cs.ovgu.de>
2018-06-03 15:26:57 +0200

    max. 1000 tp iterations

--- iter_tp.m
+++ iter_tp.m
@@ -2,10 +2,12 @@
 function x = iter_tp(theta1, theta2, outin, amin, b = 1)
   # initially, set all input literals to false, i.e. all inputs to -1
   x = -ones(size(theta1, 1)-1, 1)'
+  count = 1;
   do 
     oldx=x;
+    count += 1;
     # iterate tp
     x=tp(x,theta1, theta2, outin, amin,b)
   # until fixpoint is reached  
-  until oldx==x
+  until oldx==x || count > 1000
 endfunction
